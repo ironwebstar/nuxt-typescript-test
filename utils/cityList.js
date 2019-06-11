66eb01e43fab8ca1ce7014d31110d3105f36bed9
Artem Pavlov <artem.pavlov.ironman@gmail.com>
2019-06-11 15:25:33 +0300

    add list of cities

---
+++ utils/cityList.js
@@ -0,0 +1,710 @@
+export default [
+  {
+    'country': 'US',
+    'name': 'Bay Minette',
+    'lat': '30.88296',
+    'lng': '-87.77305'
+  },
+  {
+    'country': 'US',
+    'name': 'Edna',
+    'lat': '28.97859',
+    'lng': '-96.64609'
+  },
+  {
+    'country': 'US',
+    'name': 'Bayou La Batre',
+    'lat': '30.40352',
+    'lng': '-88.24852'
+  },
+  {
+    'country': 'US',
+    'name': 'Henderson',
+    'lat': '32.15322',
+    'lng': '-94.79938'
+  },
+  {
+    'country': 'US',
+    'name': 'Natalia',
+    'lat': '29.18968',
+    'lng': '-98.86253'
+  },
+  {
+    'country': 'US',
+    'name': 'Bear Creek',
+    'lat': '34.27482',
+    'lng': '-87.70058'
+  },
+  {
+    'country': 'US',
+    'name': 'Fort Hunt',
+    'lat': '38.73289',
+    'lng': '-77.05803'
+  },
+  {
+    'country': 'US',
+    'name': 'Yorktown',
+    'lat': '37.23876',
+    'lng': '-76.50967'
+  },
+  {
+    'country': 'US',
+    'name': 'Brighton',
+    'lat': '33.43428',
+    'lng': '-86.94721'
+  },
+  {
+    'country': 'US',
+    'name': 'Ocean View',
+    'lat': '38.54511',
+    'lng': '-75.08907'
+  },
+  {
+    'country': 'US',
+    'name': 'Berry',
+    'lat': '33.65983',
+    'lng': '-87.60001'
+  },
+  {
+    'country': 'US',
+    'name': 'Trinity',
+    'lat': '28.18085',
+    'lng': '-82.68177'
+  },
+  {
+    'country': 'US',
+    'name': 'Villas',
+    'lat': '26.55035',
+    'lng': '-81.8687'
+  },
+  {
+    'country': 'US',
+    'name': 'Bessemer',
+    'lat': '33.40178',
+    'lng': '-86.95444'
+  },
+  {
+    'country': 'US',
+    'name': 'Aurora',
+    'lat': '39.057',
+    'lng': '-84.90134'
+  },
+  {
+    'country': 'US',
+    'name': 'New Pekin',
+    'lat': '38.50506',
+    'lng': '-86.01692'
+  },
+  {
+    'country': 'US',
+    'name': 'Stockton',
+    'lat': '39.43807',
+    'lng': '-99.2651'
+  },
+  {
+    'country': 'US',
+    'name': 'Paducah',
+    'lat': '37.08339',
+    'lng': '-88.60005'
+  },
+  {
+    'country': 'US',
+    'name': 'Monticello',
+    'lat': '32.59653',
+    'lng': '-91.394'
+  },
+  {
+    'country': 'US',
+    'name': 'Red Chute',
+    'lat': '32.55598',
+    'lng': '-93.61323'
+  },
+  {
+    'country': 'US',
+    'name': 'Jessup',
+    'lat': '39.14927',
+    'lng': '-76.77525'
+  },
+  {
+    'country': 'US',
+    'name': 'Birmingham',
+    'lat': '33.52066',
+    'lng': '-86.80249'
+  },
+  {
+    'country': 'US',
+    'name': 'Delhi Hills',
+    'lat': '39.09284',
+    'lng': '-84.61272'
+  },
+  {
+    'country': 'US',
+    'name': 'Turpin Hills',
+    'lat': '39.11006',
+    'lng': '-84.37994'
+  },
+  {
+    'country': 'US',
+    'name': 'Lugoff',
+    'lat': '34.22737',
+    'lng': '-80.68925'
+  },
+  {
+    'country': 'US',
+    'name': 'Blountsville',
+    'lat': '34.08149',
+    'lng': '-86.5911'
+  },
+  {
+    'country': 'US',
+    'name': 'Blue Ridge',
+    'lat': '32.49264',
+    'lng': '-86.19052'
+  },
+  {
+    'country': 'US',
+    'name': 'Buda',
+    'lat': '30.08521',
+    'lng': '-97.84028'
+  },
+  {
+    'country': 'US',
+    'name': 'Boaz',
+    'lat': '34.20065',
+    'lng': '-86.16637'
+  },
+  {
+    'country': 'US',
+    'name': 'Brent',
+    'lat': '32.93735',
+    'lng': '-87.16472'
+  },
+  {
+    'country': 'US',
+    'name': 'Brewton',
+    'lat': '31.10518',
+    'lng': '-87.07219'
+  },
+  {
+    'country': 'US',
+    'name': 'Bridgeport',
+    'lat': '34.94758',
+    'lng': '-85.71442'
+  },
+  {
+    'country': 'US',
+    'name': 'Brook Highland',
+    'lat': '33.43566',
+    'lng': '-86.67388'
+  },
+  {
+    'country': 'US',
+    'name': 'Brookside',
+    'lat': '33.63788',
+    'lng': '-86.91666'
+  },
+  {
+    'country': 'US',
+    'name': 'Brookwood',
+    'lat': '33.25567',
+    'lng': '-87.32083'
+  },
+  {
+    'country': 'US',
+    'name': 'Brundidge',
+    'lat': '31.72016',
+    'lng': '-85.81606'
+  },
+  {
+    'country': 'US',
+    'name': 'Butler',
+    'lat': '32.08959',
+    'lng': '-88.22197'
+  },
+  {
+    'country': 'US',
+    'name': 'Bynum',
+    'lat': '33.61316',
+    'lng': '-85.96108'
+  },
+  {
+    'country': 'US',
+    'name': 'Cahaba Heights',
+    'lat': '33.464',
+    'lng': '-86.73193'
+  },
+  {
+    'country': 'US',
+    'name': 'Calera',
+    'lat': '33.1029',
+    'lng': '-86.7536'
+  },
+  {
+    'country': 'US',
+    'name': 'Camden',
+    'lat': '31.99098',
+    'lng': '-87.29055'
+  },
+  {
+    'country': 'US',
+    'name': 'Carbon Hill',
+    'lat': '33.89177',
+    'lng': '-87.52612'
+  },
+  {
+    'country': 'US',
+    'name': 'Carrollton',
+    'lat': '33.26169',
+    'lng': '-88.09503'
+  },
+  {
+    'country': 'US',
+    'name': 'Cedar Bluff',
+    'lat': '34.22009',
+    'lng': '-85.60774'
+  },
+  {
+    'country': 'US',
+    'name': 'Center Point',
+    'lat': '33.64566',
+    'lng': '-86.6836'
+  },
+  {
+    'country': 'US',
+    'name': 'Centre',
+    'lat': '34.15204',
+    'lng': '-85.67885'
+  },
+  {
+    'country': 'US',
+    'name': 'Centreville',
+    'lat': '32.9462',
+    'lng': '-87.11669'
+  },
+  {
+    'country': 'US',
+    'name': 'Chalkville',
+    'lat': '33.65316',
+    'lng': '-86.64777'
+  },
+  {
+    'country': 'US',
+    'name': 'Chatom',
+    'lat': '31.46517',
+    'lng': '-88.25446'
+  },
+  {
+    'country': 'US',
+    'name': 'Chelsea',
+    'lat': '33.34011',
+    'lng': '-86.63026'
+  },
+  {
+    'country': 'US',
+    'name': 'Cherokee',
+    'lat': '34.75703',
+    'lng': '-87.97281'
+  },
+  {
+    'country': 'US',
+    'name': 'Chickasaw',
+    'lat': '30.7638',
+    'lng': '-88.07472'
+  },
+  {
+    'country': 'US',
+    'name': 'Childersburg',
+    'lat': '33.27817',
+    'lng': '-86.35498'
+  },
+  {
+    'country': 'US',
+    'name': 'Choccolocco',
+    'lat': '33.65927',
+    'lng': '-85.70357'
+  },
+  {
+    'country': 'US',
+    'name': 'Citronelle',
+    'lat': '31.09073',
+    'lng': '-88.22806'
+  },
+  {
+    'country': 'US',
+    'name': 'Clanton',
+    'lat': '32.83874',
+    'lng': '-86.62943'
+  },
+  {
+    'country': 'US',
+    'name': 'Clay',
+    'lat': '33.7026',
+    'lng': '-86.59971'
+  },
+  {
+    'country': 'US',
+    'name': 'Clayton',
+    'lat': '31.87822',
+    'lng': '-85.44966'
+  },
+  {
+    'country': 'US',
+    'name': 'Cleveland',
+    'lat': '33.99093',
+    'lng': '-86.57749'
+  },
+  {
+    'country': 'US',
+    'name': 'Clio',
+    'lat': '31.70878',
+    'lng': '-85.6105'
+  },
+  {
+    'country': 'US',
+    'name': 'Coaling',
+    'lat': '33.15901',
+    'lng': '-87.34083'
+  },
+  {
+    'country': 'US',
+    'name': 'Collinsville',
+    'lat': '34.26398',
+    'lng': '-85.86053'
+  },
+  {
+    'country': 'US',
+    'name': 'Columbiana',
+    'lat': '33.17817',
+    'lng': '-86.60721'
+  },
+  {
+    'country': 'US',
+    'name': 'Concord',
+    'lat': '33.46761',
+    'lng': '-87.03111'
+  },
+  {
+    'country': 'US',
+    'name': 'Coosada',
+    'lat': '32.49791',
+    'lng': '-86.33136'
+  },
+  {
+    'country': 'US',
+    'name': 'Cordova',
+    'lat': '33.75983',
+    'lng': '-87.18333'
+  },
+  {
+    'country': 'US',
+    'name': 'Cottonwood',
+    'lat': '31.04879',
+    'lng': '-85.30493'
+  },
+  {
+    'country': 'US',
+    'name': 'Cowarts',
+    'lat': '31.20018',
+    'lng': '-85.30465'
+  },
+  {
+    'country': 'US',
+    'name': 'Creola',
+    'lat': '30.89185',
+    'lng': '-88.03972'
+  },
+  {
+    'country': 'US',
+    'name': 'Crossville',
+    'lat': '34.28759',
+    'lng': '-85.99414'
+  },
+  {
+    'country': 'US',
+    'name': 'Cullman',
+    'lat': '34.17482',
+    'lng': '-86.84361'
+  },
+  {
+    'country': 'US',
+    'name': 'Dadeville',
+    'lat': '32.83124',
+    'lng': '-85.76357'
+  },
+  {
+    'country': 'US',
+    'name': 'Daleville',
+    'lat': '31.31017',
+    'lng': '-85.71299'
+  },
+  {
+    'country': 'US',
+    'name': 'Danville',
+    'lat': '34.41454',
+    'lng': '-87.08751'
+  },
+  {
+    'country': 'US',
+    'name': 'Daphne',
+    'lat': '30.60353',
+    'lng': '-87.9036'
+  },
+  {
+    'country': 'US',
+    'name': 'Dauphin Island',
+    'lat': '30.25548',
+    'lng': '-88.10972'
+  },
+  {
+    'country': 'US',
+    'name': 'Deatsville',
+    'lat': '32.60819',
+    'lng': '-86.39581'
+  },
+  {
+    'country': 'US',
+    'name': 'Decatur',
+    'lat': '34.60593',
+    'lng': '-86.98334'
+  },
+  {
+    'country': 'US',
+    'name': 'Demopolis',
+    'lat': '32.51764',
+    'lng': '-87.8364'
+  },
+  {
+    'country': 'US',
+    'name': 'Dora',
+    'lat': '33.72872',
+    'lng': '-87.09028'
+  },
+  {
+    'country': 'US',
+    'name': 'Dothan',
+    'lat': '31.22323',
+    'lng': '-85.39049'
+  },
+  {
+    'country': 'US',
+    'name': 'Double Springs',
+    'lat': '34.14637',
+    'lng': '-87.40247'
+  },
+  {
+    'country': 'US',
+    'name': 'East Brewton',
+    'lat': '31.09323',
+    'lng': '-87.06275'
+  },
+  {
+    'country': 'US',
+    'name': 'East Florence',
+    'lat': '34.80953',
+    'lng': '-87.64947'
+  },
+  {
+    'country': 'US',
+    'name': 'Eclectic',
+    'lat': '32.63541',
+    'lng': '-86.03441'
+  },
+  {
+    'country': 'US',
+    'name': 'Elberta',
+    'lat': '30.41436',
+    'lng': '-87.59776'
+  },
+  {
+    'country': 'US',
+    'name': 'Enterprise',
+    'lat': '31.31517',
+    'lng': '-85.85522'
+  },
+  {
+    'country': 'US',
+    'name': 'Eufaula',
+    'lat': '31.89127',
+    'lng': '-85.14549'
+  },
+  {
+    'country': 'US',
+    'name': 'Eutaw',
+    'lat': '32.84059',
+    'lng': '-87.88762'
+  },
+  {
+    'country': 'US',
+    'name': 'Evergreen',
+    'lat': '31.4335',
+    'lng': '-86.95692'
+  },
+  {
+    'country': 'US',
+    'name': 'Fairfield',
+    'lat': '33.48594',
+    'lng': '-86.91194'
+  },
+  {
+    'country': 'US',
+    'name': 'Fairhope',
+    'lat': '30.52297',
+    'lng': '-87.90333'
+  },
+  {
+    'country': 'US',
+    'name': 'Falkville',
+    'lat': '34.36843',
+    'lng': '-86.90862'
+  },
+  {
+    'country': 'US',
+    'name': 'Fayette',
+    'lat': '33.68455',
+    'lng': '-87.83085'
+  },
+  {
+    'country': 'US',
+    'name': 'Fayetteville',
+    'lat': '33.14567',
+    'lng': '-86.40581'
+  },
+  {
+    'country': 'US',
+    'name': 'Flint City',
+    'lat': '34.52315',
+    'lng': '-86.97029'
+  },
+  {
+    'country': 'US',
+    'name': 'Florala',
+    'lat': '31.00518',
+    'lng': '-86.328'
+  },
+  {
+    'country': 'US',
+    'name': 'Florence',
+    'lat': '34.79981',
+    'lng': '-87.67725'
+  },
+  {
+    'country': 'US',
+    'name': 'Flomaton',
+    'lat': '31.00018',
+    'lng': '-87.26081'
+  },
+  {
+    'country': 'US',
+    'name': 'Foley',
+    'lat': '30.40659',
+    'lng': '-87.6836'
+  },
+  {
+    'country': 'US',
+    'name': 'Forestdale',
+    'lat': '33.57011',
+    'lng': '-86.89638'
+  },
+  {
+    'country': 'US',
+    'name': 'Fort Deposit',
+    'lat': '31.98459',
+    'lng': '-86.57859'
+  },
+  {
+    'country': 'US',
+    'name': 'Fort Payne',
+    'lat': '34.44425',
+    'lng': '-85.71969'
+  },
+  {
+    'country': 'US',
+    'name': 'Frisco City',
+    'lat': '31.4335',
+    'lng': '-87.40138'
+  },
+  {
+    'country': 'US',
+    'name': 'Fultondale',
+    'lat': '33.60483',
+    'lng': '-86.79388'
+  },
+  {
+    'country': 'US',
+    'name': 'Fyffe',
+    'lat': '34.44676',
+    'lng': '-85.90414'
+  },
+  {
+    'country': 'US',
+    'name': 'Gadsden',
+    'lat': '34.01434',
+    'lng': '-86.00639'
+  },
+  {
+    'country': 'US',
+    'name': 'Gardendale',
+    'lat': '33.6601',
+    'lng': '-86.81277'
+  },
+  {
+    'country': 'US',
+    'name': 'Geneva',
+    'lat': '31.03296',
+    'lng': '-85.86382'
+  },
+  {
+    'country': 'US',
+    'name': 'Georgiana',
+    'lat': '31.6371',
+    'lng': '-86.74192'
+  },
+  {
+    'country': 'US',
+    'name': 'Glencoe',
+    'lat': '33.95704',
+    'lng': '-85.93247'
+  },
+  {
+    'country': 'US',
+    'name': 'Good Hope',
+    'lat': '34.11593',
+    'lng': '-86.86361'
+  },
+  {
+    'country': 'US',
+    'name': 'Goodwater',
+    'lat': '33.06567',
+    'lng': '-86.0533'
+  },
+  {
+    'country': 'US',
+    'name': 'Gordo',
+    'lat': '33.32012',
+    'lng': '-87.9028'
+  },
+  {
+    'country': 'US',
+    'name': 'Grand Bay',
+    'lat': '30.47631',
+    'lng': '-88.34223'
+  },
+  {
+    'country': 'US',
+    'name': 'Grayson Valley',
+    'lat': '33.64816',
+    'lng': '-86.63943'
+  },
+  {
+    'country': 'US',
+    'name': 'Graysville',
+    'lat': '33.62066',
+    'lng': '-86.97138'
+  },
+  {
+    'country': 'US',
+    'name': 'Greensboro',
+    'lat': '32.70415',
+    'lng': '-87.5955'
+  }
+]
