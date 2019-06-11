396a401d664c34e2fd0d5fffeee5a2022b8de7c4
Artem Pavlov <artem.pavlov.ironman@gmail.com>
2019-06-11 15:30:37 +0300

    add actions in store

--- store/index.ts
+++ store/index.ts
@@ -3,7 +3,14 @@ import Example from './modules/example'
 
 const createStore = () => {
   return new Vuex.Store({
-    state: {},
+    state: {
+      cities: []
+    },
+    actions: {
+      setCities(context) {
+        context.state.cities = JSON.parse(localStorage.getItem('cities'))
+      }
+    },
     modules: {
       [Example.NAME]: Example
     }
